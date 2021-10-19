5835ea0a59087b305ce135d81228509516da90e9
liutao <liutao@deepexi.com>
2021-10-19 10:58:47 +0800

    feat: 注入

--- element-ui/装饰器/injector.ts
+++ element-ui/装饰器/injector.ts
@@ -1,15 +1,21 @@
+import 'reflect-metadata'
 const mapInstance = new Map()
 const mapTarget = new Map()
-export function Injectable(key: string) {
+export function Injectable(key?: string) {
   return function(target: any) {
     if (mapTarget.has(key)) {
       throw new Error(`Injector中${key}重名`)
     }
-    mapTarget.set(key, target)
+    key !== undefined && mapTarget.set(key, target)
+    mapTarget.set(target, target)
   }
 }
-export function Injector(key: string) {
+export function Injector(key?: string) {
   return function(target: any, propertyKey: string) {
+    if (key === undefined) {
+      key = Reflect.getMetadata('design:type', target, propertyKey)
+    }
+
     if (mapTarget.has(key)) {
       if (mapInstance.has(key)) {
         target[propertyKey] = mapInstance.get(key)
